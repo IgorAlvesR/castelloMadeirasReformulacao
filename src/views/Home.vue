41efd61fea1e1fbb4fa5bce7fe1023fdfd763e29
igor.rodrigues <igor@cincatarina.sc.gov.br>
2022-09-14 14:34:04 -0300

    feat: insere botão de wpp para entrar em contato

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <Content />
+    <v-btn class="button-float" dark fab bottom right>
+      <span class="">
+        <a
+          class="card-text__link"
+          href="https://wa.me/5548998244494?text=Ol%C3%A1%2C+gostaria+de+solicitar+um+or%C3%A7amento%21"
+          target="_blank"
+        >
+          <v-icon color="green">mdi-whatsapp</v-icon>
+        </a>
+      </span>
+    </v-btn>
   </div>
 </template>
 
@@ -12,3 +23,27 @@ export default {
   components: { Content }
 }
 </script>
+<style scoped>
+.button-float {
+}
+.card-text__link {
+  text-decoration: none;
+}
+.button-float {
+  padding: 10px;
+  margin: 0 10px;
+  display: block;
+  transition: 0.3s;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+}
+
+.button-float .v-icon {
+  font-size: 1.7rem;
+}
+
+.button-float:hover {
+  transform: translateY(-0.35rem);
+}
+</style>
