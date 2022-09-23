6e6cca752eac79b8a8841d75c8d45cf9ab7daaaa
igor.rodrigues <igor@cincatarina.sc.gov.br>
2022-09-23 10:00:29 -0300

    feat: insere animação do logo no quem somos

--- src/components/QuemSomos.vue
+++ src/components/QuemSomos.vue
@@ -7,16 +7,48 @@
     outlined
     tile
   >
-    <v-container class="pt-16 pb-16">
-      <v-card-title class="red-text title pa-0 pb-4">Quem somos</v-card-title>
-      <v-card-text class="subtitle-1 pa-0"
-        >A Castello Madeiras é uma empresa situada em Tubarão-SC que atua no
-        ramo de construção de casa pré-fabricada de madeira, elaboramos seu
-        projeto personalizado, com uma equipe qualificada para atender a
-        necessidade da sua família com o novo lar. Casas modernas, na cidade, no
-        campo e na praia. Agradáveis, arejadas no verão e aconchegantes no
-        inverno.
-      </v-card-text>
-    </v-container>
+    <v-row class="mt-4 mb-4 d-flex justify-center">
+      <v-col sm="4" cols="12" class="d-flex align-center">
+        <v-container class="d-flex justify-center">
+          <v-img
+            class="quem-somos-img"
+            max-height="150"
+            max-width="200"
+            src="@/assets/quemsomos.png"
+          ></v-img>
+        </v-container>
+      </v-col>
+      <v-col sm="6" cols="12">
+        <v-container class="pt-16 pb-16">
+          <v-card-title class="red-text title"> Quem somos </v-card-title>
+          <v-card-text class="subtitle-1">
+            A Castello Madeiras é uma empresa situada em Tubarão-SC que atua no
+            ramo de construção de casa pré-fabricada de madeira, elaboramos seu
+            projeto personalizado, com uma equipe qualificada para atender a
+            necessidade da sua família com o novo lar. Casas modernas, na
+            cidade, no campo e na praia. Agradáveis, arejadas no verão e
+            aconchegantes no inverno.
+          </v-card-text>
+        </v-container>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
+
+<style scoped>
+@keyframes scroll {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-0.8rem);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.quem-somos-img {
+  animation: scroll 2s infinite;
+}
+</style>
