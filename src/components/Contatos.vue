f1253f6aeb2a007108e92f8749ddbbd7efdeef41
igor.rodrigues <igor@cincatarina.sc.gov.br>
2023-01-23 16:11:03 -0300

    feat: remove contato e ajusta grid

--- src/components/Contatos.vue
+++ src/components/Contatos.vue
@@ -1,15 +1,20 @@
 <template>
   <v-row id="contato">
-    <v-col cols="12" class="pt-0 pb-0">
+    <v-col cols="12" class="pt-0 pb-0 pl-0 pr-0">
       <v-card dark tile>
         <v-card-title id="contato" class="red-text justify-center"
           >Contato</v-card-title
         >
       </v-card>
     </v-col>
-    <v-col cols="12" sm="6" class="pa-0">
-      <v-card elevation="2" tile dark class="text-center pl-8 pr-8">
-        <v-card-text>
+    <v-col cols="12" class="pa-0">
+      <v-card
+        elevation="2"
+        tile
+        dark
+        class="text-center pl-8 pr-8 d-flex flex-column align-center"
+      >
+        <v-card-text class="max-width-500">
           <span class="card-text__numbers">
             <a
               class="card-text__link"
@@ -21,7 +26,7 @@
             (48) 99824-4494
           </span>
         </v-card-text>
-        <v-card-text>
+        <v-card-text class="max-width-500">
           <span class="card-text__numbers">
             <a
               class="card-text__link"
@@ -33,20 +38,10 @@
             (48) 99857-2530
           </span>
         </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols="12" sm="6" class="pa-0">
-      <v-card elevation="2" tile dark class="text-center pl-8 pr-8">
-        <v-card-text>
+        <v-card-text class="max-width-500">
           <span class="card-text__numbers">
             <v-icon color="red">mdi-phone</v-icon>
-            (48) 3632-8743 - Tubarão
-          </span>
-        </v-card-text>
-        <v-card-text>
-          <span class="card-text__numbers">
-            <v-icon color="red">mdi-phone</v-icon>
-            (48) 3632-8743 - Imbituba
+            (48) 3632-8743
           </span>
         </v-card-text>
       </v-card>
